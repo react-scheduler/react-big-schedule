a3e2ff0891fb0cc123876885cf426572cb6d2143
Ansul Agrawal <ansulagrawal9@gmail.com>
2023-05-10 20:09:36 +0530

    upaded config

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,22 @@
-// import path from 'path';
-import { resolve } from 'node:path';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve('src', 'components/index.jsx'),
+      entry: path.resolve(__dirname, 'src/components/index.jsx'),
       name: 'react-scheduler-master',
-      formats: ['es', 'umd'],
-      fileName: format => `react-scheduler-master.${format}.js`,
+      fileName: (format) => `react-scheduler-master.${format}.js`
     },
     rollupOptions: {
-      external: ['react', 'react-dom', 'antd', 'moment', 'react-dnd', 'react-dnd-html5-backend', 'rrule', '@ant-design/icons', 'prop-types'],
+      external: ['react', 'react-dom'],
       output: {
         globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          antd: 'antd',
-          moment: 'moment',
-          '@ant-design/icons': 'antdIcons',
-          'prop-types': 'PropTypes',
-          'react-dnd': 'ReactDnd',
-          'react-dnd-html5-backend': 'ReactDndHtml5Backend',
-          rrule: 'rrule',
-        },
-      },
-    },
+          react: 'React'
+        }
+      }
+    }
   },
-  plugins: [react()],
-});
+  plugins: [react()]
+})
