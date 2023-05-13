ec3990c1e3697daa2c9e7de014fd86a5c4c4ecd3
Ansul Agrawal <ansulagrawal9@gmail.com>
2023-05-13 14:40:15 +0530

    Added semantic-release

---
+++ release.config.js
@@ -0,0 +1,17 @@
+const config = {
+  branches: ['master'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['dist/*.js', 'dist/*.js.map', 'dist/*.css', 'dist/*.svg'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
+  ],
+};
+
+module.exports = config;
