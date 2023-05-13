f28b97888885fbe9c796baa1f117cdc29393fe8d
Ansul Agrawal <ansulagrawal9@gmail.com>
2023-05-14 00:19:20 +0530

    Feat: Added New Feature - Add Week, Month and Year cell size
    BREAKING CHANGE: Added New Feature i.e. Add Week, Month and Year cell units

--- src/config/default.js
+++ src/config/default.js
@@ -4,7 +4,7 @@ export const DATETIME_FORMAT = 'YYYY-MM-DD HH:mm:ss';
 
 export const ViewTypes = { Day: 0, Week: 1, Month: 2, Quarter: 3, Year: 4, Custom: 5, Custom1: 6, Custom2: 7 };
 
-export const CellUnits = { Day: 0, Hour: 1 };
+export const CellUnits = { Day: 0, Hour: 1, Week: 2, Month: 3, Year: 4 };
 
 export const SummaryPos = { Top: 0, TopRight: 1, TopLeft: 2, Bottom: 3, BottomRight: 4, BottomLeft: 5 };
 
