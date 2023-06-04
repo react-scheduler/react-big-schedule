f5035e626df9d0cc31f28defe8027f229edbb7d8
Ansul Agrawal <ansulagrawal9@gmail.com>
2023-06-04 18:41:41 +0530

    Feat: Added Coming Soon Example Breaking Release: Added New example of coming soon

---
+++ src/examples/ComingSoon.jsx
@@ -0,0 +1,7 @@
+import { Result } from 'antd';
+
+function ComingSoon() {
+  return <Result status="404"  title='Sorry, the example is on the way!' style={{marginTop: '25vh'}} />;
+}
+
+export default ComingSoon;
