01e45ae7269a57545ddd13e9dadec3a77ec5a134
Ansul Agrawal <ansulagrawal9@gmail.com>
2023-07-13 23:32:05 +0530

    Fix Function name

--- src/components/WrapperFun.jsx
+++ src/components/WrapperFun.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
-const withDragAndDrop = Component => props => (
+const wrapperFun = Component => props => (
   <DndProvider backend={HTML5Backend}>
     <Component {...props} />
   </DndProvider>
 );
 
-export default withDragAndDrop;
+export default wrapperFun;
