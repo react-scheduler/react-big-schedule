770c40ab9a43f21c718224713d1bf52746552d65
Ansul Agrawal <ansulagrawal9@gmail.com>
2023-08-04 00:05:43 +0530

    Optimize renderViewEvent

--- src/components/EventItemPopover.jsx
+++ src/components/EventItemPopover.jsx
@@ -28,76 +28,70 @@ const EventItemPopover = ({
   const showViewEvent = viewEventText && viewEventClick && (eventItem.clickable1 === undefined || eventItem.clickable1);
   const showViewEvent2 = viewEvent2Text && viewEvent2Click && (eventItem.clickable2 === undefined || eventItem.clickable2);
 
-  const renderViewEvent = () => (
-    <span className='header2-text' style={{ color: '#108EE9', cursor: 'pointer' }} onClick={() => viewEventClick(schedulerData, eventItem)}>
-      {viewEventText}
-    </span>
-  );
-
-  const renderViewEvent2 = () => (
-    <span className='header2-text' style={{ color: '#108EE9', cursor: 'pointer', marginLeft: '16px' }} onClick={() => viewEvent2Click(schedulerData, eventItem)}>
-      {viewEvent2Text}
+  const renderViewEvent = (text, clickHandler, marginLeft = 0) => (
+    <span className="header2-text" style={{ color: '#108EE9', cursor: 'pointer', marginLeft: `${marginLeft}px` }} onClick={() => clickHandler(schedulerData, eventItem)}>
+      {text}
     </span>
   );
 
   return (
     <div style={{ width: config.eventItemPopoverWidth }}>
-      <Row type='flex' align='middle'>
+      <Row type="flex" align="middle">
         {config.eventItemPopoverShowColor && (
           <Col span={2}>
-            <div className='status-dot' style={{ backgroundColor: statusColor }} />
+            <div className="status-dot" style={{ backgroundColor: statusColor }} />
           </Col>
         )}
-        <Col span={22} className='overflow-text'>
-          <span className='header2-text' title={title}>
+        <Col span={22} className="overflow-text">
+          <span className="header2-text" title={title}>
             {`${title}`}
           </span>
         </Col>
       </Row>
       {subtitle && (
-        <Row type='flex' align='middle'>
+        <Row type="flex" align="middle">
           <Col span={2}>
             <div />
           </Col>
-          <Col span={22} className='overflow-text'>
-            <span className='header2-text' title={subtitle}>
+          <Col span={22} className="overflow-text">
+            <span className="header2-text" title={subtitle}>
               {subtitle}
             </span>
           </Col>
         </Row>
       )}
-      <Row type='flex' align='middle'>
+      <Row type="flex" align="middle">
         <Col span={2}>
           <div />
         </Col>
         <Col span={22}>
-          <span className='header1-text'>{start.format('HH:mm')}</span>
+          <span className="header1-text">{start.format('HH:mm')}</span>
           {config.eventItemPopoverDateFormat && (
-            <span className='help-text' style={{ marginLeft: '8px' }}>
+            <span className="help-text" style={{ marginLeft: '8px' }}>
               {start.format(config.eventItemPopoverDateFormat)}
             </span>
           )}
-          <span className='header2-text' style={{ marginLeft: '8px' }}>
+          <span className="header2-text" style={{ marginLeft: '8px' }}>
             -
           </span>
-          <span className='header1-text' style={{ marginLeft: '8px' }}>
+          <span className="header1-text" style={{ marginLeft: '8px' }}>
             {end.format('HH:mm')}
           </span>
           {config.eventItemPopoverDateFormat && (
-            <span className='help-text' style={{ marginLeft: '8px' }}>
+            <span className="help-text" style={{ marginLeft: '8px' }}>
               {end.format(config.eventItemPopoverDateFormat)}
             </span>
           )}
         </Col>
       </Row>
       {(showViewEvent || showViewEvent2) && (
-        <Row type='flex' align='middle'>
+        <Row type="flex" align="middle">
           <Col span={2}>
             <div />
           </Col>
           <Col span={22}>
-            {showViewEvent && renderViewEvent()}
-            {showViewEvent2 && renderViewEvent2()}
+            {showViewEvent && renderViewEvent(viewEventText, viewEventClick)}
+            {showViewEvent2 && renderViewEvent(viewEvent2Text, viewEvent2Click, 16)}
           </Col>
         </Row>
       )}
