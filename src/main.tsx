ca83f8ca446a67a84d8cb42efb405e86c1ed2df2
Pedro Roque <pedro.roque@muzz.com>
2024-09-12 16:11:07 +0100

    Added typescript package and supporting typescript configuration.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
+// @ts-nocheck
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './examples/AddMore';
 import './css/style.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(<App />);
+}
--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ESLintWebpackPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-  entry: './src/examples/index.jsx',
+  entry: './src/examples/index.tsx',
   output: {
     path: path.resolve(__dirname, '..', 'dist'),
     filename: 'bundle.js',
@@ -12,7 +12,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.(js|jsx|ts|tsx)$/,
         exclude: /node_modules/,
         use: { loader: 'babel-loader' },
       },
@@ -20,14 +20,19 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
     ],
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
   plugins: [
     new HtmlWebpackPlugin({ template: './src/index.html', title: 'React Big Schedule' }),
-    new ESLintWebpackPlugin({ emitError: true, emitWarning: false, failOnError: true, extensions: ['js', 'jsx'] }),
+    new ESLintWebpackPlugin({ emitError: true, emitWarning: false, failOnError: true, extensions: ['js', 'jsx', 'ts', 'tsx'] }),
   ],
   devServer: {
     static: { directory: path.join(__dirname, '..', 'dist') },
