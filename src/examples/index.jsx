546790e09aa6162dbe880b95b81722e429063ac5
Ansul Agrawal <ansul.agrawal@generalaeronautics.com>
2024-10-12 04:02:09 +0530

    Updated Build Script and Change from webpack to vite

--- src/examples/index.jsx
+++ src/examples/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app';
-
-createRoot(document.getElementById('app')).render(<App />);
+createRoot(document.getElementById('root')).render(<App />);
